a76749e821648dc05f4d77a7ea8033e20cf9e438
TheIPM <islamluse@gmail.com>
2023-05-07 16:12:37 +0930

    added downloadable resume

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
 import 'animate.css';
 import TrackVisibility from 'react-on-screen';
+import ResumeFile from '../assets/Islam M Resume 2023.pdf';
+
 
 const Resume = () => {
   const skills = [
@@ -30,6 +32,10 @@ const Resume = () => {
                     <li key={index}>{skill}</li>
                   ))}
                 </ul>
+                <h3>Download Resume</h3>
+                <a href={ResumeFile} download="Islam_M_Resume_2023.pdf">
+                <button type="button" style={{ color: 'white' }}>Click Here</button>
+                </a>
               </Col>
             </Row>
           )}
