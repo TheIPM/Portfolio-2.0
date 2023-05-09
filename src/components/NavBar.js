1cbb9e154154640db2bb35880a922005583c4ccb
TheIPM <islamluse@gmail.com>
2023-05-09 19:16:11 +0930

    Updated logo so its a link

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -34,9 +34,9 @@ export const NavBar = () => {
   return (
       <Navbar expand="md" className={scrolled ? "scrolled" : ""}>
         <Container>
-          <Navbar.Brand href="/">
-            <img src={logo} alt="Logo" />
-          </Navbar.Brand>
+        <Navbar.Brand as={Link} to="/">
+          <img src={logo} alt="Logo" />
+        </Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav">
             <span className="navbar-toggler-icon"></span>
           </Navbar.Toggle>
